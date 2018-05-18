e7c074f2a051885bb0f9992471460763bc7ce0f0
Cody Choi <cchoi_2294@hotmail.com>
2018-05-18 15:42:36 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.post('/todos', (req, res) => {
     const todo = new Todo ({
         text: req.body.text
     });
+    console.log(todo)
 
     todo.save().then((doc) => {
         res.send(doc)
@@ -21,6 +22,18 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        })
+    }, (err) => {
+        res.state(400).send(err)
+    })
+})
+
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
+
+module.exports = {app}
