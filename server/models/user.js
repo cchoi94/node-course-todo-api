6294b600233803bb1697570962be8508a8ac994e
Cody Choi <cchoi_2294@hotmail.com>
2018-05-25 16:36:51 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -86,6 +86,26 @@ UserSchema.statics.findByToken = function (token) { //.statics create model meth
     })
 } 
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    const User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject()
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res === true) {
+                    resolve(user)
+                } else {
+                    reject();
+                }
+            })
+        })
+    })
+}
+
 UserSchema.pre('save', function(next) {
     var user = this;
 
